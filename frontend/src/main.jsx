0016994039484c0c1ea440d49365530d2ae32d0f
Oksana <ksenia.tkachenko.ua@gmail.com>
2024-12-26 17:06:45 +0700

    add Rollbar integration for error tracking in React app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,30 @@ import App from './App.jsx'
 import store from './store/store.js';
 import './i18n';
 import i18nInit from '@src/i18n';
+import { RollbarProvider } from "@rollbar/react";
 
+const rollbarConfig = {
+  accessToken: "c0bc328e8dca445d974c741e40d55e5f", 
+  environment: "production",
+};
+
+function TestError() {
+  const a = null;
+  return a.hello();
+}
 
 const startApp = async () => {
   await i18nInit();
 
   createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>,
+    <RollbarProvider config={rollbarConfig}>
+      <StrictMode>
+        <Provider store={store}>
+          <App />
+          <TestError />
+        </Provider>
+        </StrictMode>
+    </RollbarProvider>
   );
 };
 
