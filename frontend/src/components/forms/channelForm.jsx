e52c602e00332c69dd82169bab31ad519783ae03
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-01-28 15:03:24 +0700

    fix eslint erors

--- frontend/src/components/forms/channelForm.jsx
+++ frontend/src/components/forms/channelForm.jsx
@@ -1,95 +1,3 @@
-// import {
-//   Formik,
-//   Form,
-//   Field,
-//   ErrorMessage
-// } from 'formik';
-// import React, { useEffect } from 'react';
-// import { t } from '@utils/i18n';
-// import getValidationSchema from '@utils/validationSchema';
-
-// const ChannelForm = ({ actionInfo, onHide, action, inputChatRef, chatChannels }) => {
-//   const { title, submitBtnTitle, initialChannelName, onSubmit } = actionInfo;
-
-//   useEffect(() => {
-//     if (action === 'delete') {
-//       return;
-//     }
-//     inputChatRef.current.focus();
-//     inputChatRef.current.select();
-//   }, []);
-
-//   return (
-//     <Formik
-//       initialValues={{ channelName: initialChannelName }}
-//       enableReinitialize
-//       validationSchema={getValidationSchema('chatForm', action, chatChannels)}
-//       validateOnChange={false}
-//       validateOnBlur={false}
-//       onSubmit={async (values, { resetForm }) => {
-//         await onSubmit(values);
-//         resetForm();
-//         onHide();
-//       }}>
-//       {({ errors, touched, resetForm }) => {
-//         return (
-//           <Form>
-//             <div className="modal-header">
-//               <h5 className="modal-title">{title}</h5>
-//               <button
-//                 type="button"
-//                 className="btn-close"
-//                 aria-label="Close"
-//                 onClick={() => {
-//                   resetForm();
-//                   onHide();
-//                 }}
-//               />
-//             </div>
-//             <div className="modal-body">
-//               {action !== 'delete' && (
-//                 <>
-//                   <label htmlFor="channelName" className="visually-hidden">
-//                     {t('channels.channelName')}
-//                   </label>
-//                   <Field
-//                     innerRef={inputChatRef}
-//                     id="channelName"
-//                     name="channelName"
-//                     type="text"
-//                     autoFocus
-//                     className={`form-control ${errors.channelName && touched.channelName ? 'is-invalid' : ''}`}
-//                   />
-
-//                   <ErrorMessage name="channelName" component="div" className="invalid-feedback" />
-//                 </>
-//               )}
-//               {action === 'delete' && <p className="lead">{t('general.areYouSure')}</p>}
-//             </div>
-//             <div className="modal-footer">
-//               <button
-//                 type="button"
-//                 className="btn btn-secondary me-2"
-//                 onClick={() => {
-//                   resetForm();
-//                   onHide();
-//                 }}>
-//                 {t('general.cancel')}
-//               </button>
-//               <button type="submit" className={`btn ${action === 'delete' ? 'btn-danger' : 'btn-primary'}`}>
-//                 {submitBtnTitle}
-//               </button>
-//             </div>
-//           </Form>
-//         );
-//       }}
-//     </Formik>
-//   );
-// };
-
-// export default ChannelForm;
-
-
 import {
   Formik,
   Form,
@@ -100,18 +8,18 @@ import React, { useEffect } from 'react';
 import { t } from '@utils/i18n';
 import getValidationSchema from '@utils/validationSchema';
 
-const ChannelForm = ({ 
-  actionInfo, 
-  onHide, 
-  action, 
-  inputChatRef, 
-  chatChannels 
+const ChannelForm = ({
+  actionInfo,
+  onHide,
+  action,
+  inputChatRef,
+  chatChannels,
 }) => {
-  const { 
-    title, 
-    submitBtnTitle, 
-    initialChannelName, 
-    onSubmit 
+  const {
+    title,
+    submitBtnTitle,
+    initialChannelName,
+    onSubmit,
   } = actionInfo;
 
   useEffect(() => {
@@ -120,7 +28,7 @@ const ChannelForm = ({
     }
     inputChatRef.current.focus();
     inputChatRef.current.select();
-  }, [action, inputChatRef]); 
+  }, [action, inputChatRef]);
 
   return (
     <Formik
--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,121 +1,20 @@
-// import avatarImg from '@assets/avatar-DIE1AEpS.jpg';
-// import {
-//   useDispatch,
-//   useSelector
-// } from 'react-redux';
-// import { loginUser } from '@slices/authSlice';
-// import {
-//   useNavigate,
-//   Link
-// } from 'react-router-dom';
-// import MainCard from '@components/mainCard';
-// import {
-//   Formik,
-//   Form,
-//   Field,
-//   ErrorMessage
-// } from 'formik';
-// import { t } from '@utils/i18n.js';
-// import getValidationSchema from '@utils/validationSchema';
-// import routes from '@utils/routes';
-
-// const LoginForm = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const loadingStatus = useSelector((state) => state.loadingStatus);
-
-//   const onSubmit = async (values, { setErrors }) => {
-//     try {
-//       const response = await dispatch(loginUser(values)).unwrap();
-//       if (response.token) {
-//         navigate(routes.home);
-//       }
-//     } catch {
-//       setErrors({
-//         password: t('auth.invalidCredentials'),
-//         username: ' ',
-//       });
-//     }
-//   };
-
-//   return (
-//     <Formik
-//       initialValues={{ username: '', password: '' }}
-//       validateOnBlur={false}
-//       validationSchema={getValidationSchema('loginPage')}
-//       onSubmit={onSubmit}>
-//       {({ errors, touched }) => (
-//         <Form className="text-center">
-//           <h1 className="mb-4">{t('auth.login')}</h1>
-//           <div className="form-floating mb-3">
-//             <Field
-//               autoFocus
-//               id="username"
-//               name="username"
-//               type="text"
-//               placeholder={t('auth.usernameLabel')}
-//               className={`form-control ${errors.username && touched.username ? 'is-invalid' : ''}`}
-//             />
-//             <label htmlFor="username">{t('auth.usernameLabel')}</label>
-//             <ErrorMessage name="username" component="div" className="invalid-feedback" />
-//           </div>
-//           <div className="form-floating mb-4">
-//             <Field
-//               id="password"
-//               name="password"
-//               type="password"
-//               placeholder={t('auth.password')}
-//               className={`form-control ${errors.password && touched.password ? 'is-invalid' : ''}`}
-//             />
-//             <label htmlFor="password">{t('auth.password')}</label>
-//             <ErrorMessage name="password" component="div" className="invalid-feedback" />
-//           </div>
-//           <button type="submit"
-//  disabled={loadingStatus === 'loading'} className="btn btn-outline-primary mb-3 w-100">
-//             {t('auth.login')}
-//           </button>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// };
-
-// const LoginPage = () => {
-//   const cardFooter = (
-//     <div className="card-footer p-4">
-//       <div className="text-center">
-//         <span>{t('auth.noAccount')} </span>
-//         <Link to={routes.signup}>{t('auth.register')}</Link>
-//       </div>
-//     </div>
-//   );
-
-//   return (
-//     <MainCard img={avatarImg} cardFooter={cardFooter}>
-//       <LoginForm />
-//     </MainCard>
-//   );
-// };
-
-// export default LoginPage;
-
 import avatarImg from '@assets/avatar-DIE1AEpS.jpg';
 import {
   useDispatch,
   useSelector,
-} from 'react-redux'; // Добавлена запятая для `comma-dangle`
+} from 'react-redux';
 import { loginUser } from '@slices/authSlice';
 import {
   useNavigate,
   Link,
-} from 'react-router-dom'; // Добавлена запятая для `comma-dangle`
+} from 'react-router-dom';
 import MainCard from '@components/mainCard';
 import {
   Formik,
   Form,
   Field,
   ErrorMessage,
-} from 'formik'; // Добавлена запятая для `comma-dangle`
+} from 'formik'; 
 import { t } from '@utils/i18n.js';
 import getValidationSchema from '@utils/validationSchema';
 import routes from '@utils/routes';
